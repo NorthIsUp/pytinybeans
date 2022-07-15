610a8bcf41277402c3b1881ed8a1f1a1fcd7ebf6
Adam Hitchcock <adam@northisup.com>
2022-07-15 11:15:37 -0700

    formatt

--- setup.py
+++ setup.py
@@ -5,25 +5,21 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pytinybeans',
-    version='1.0',
-    author="Kevin O'Connor",
-    author_email='kjoconnor@gmail.com',
-    description='Python library to interact with the Tinybeans API',
+    version='1.0.1',
+    author='Adam Hitchcock',
+    author_email='adam@northisup.com',
+    description='Asyncio Python library to interact with the Tinybeans API',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/kjoconnor/pytinybeans',
+    url='https://github.com/northisup/pytinybeans',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
     ],
     python_requires='>=3.9',
-    install_requires=[
-        'requests',
-        'inflection',
-        'pydantic'
-    ],
+    install_requires=['requests', 'inflection', 'pydantic', 'aiohttp'],
     tests_require=[
         'pytest',
-    ]
+    ],
 )
